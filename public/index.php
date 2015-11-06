3e530cfefcf185ce2c9695033678f5801ac28a09
Zach S <zach.saunders@CIT3-101-15WX.auckland.natcoll.ac.nz>
2015-11-05 18:50:46 -0800

    Inital project

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+    <title>Stories at sea home</title>
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="stories at sea landing page" />
+    <meta name="keywords" content="stories, forums, sea, sailor, sailing, boat, ship, deck, tradegy, ocean, salt, anchor, str'bd, port, aft, bow, bridge" />
+    <meta name="author" content="Zach Saunders" />
+    <link href="./stylesheets/main.css" type="text/css" rel="stylesheet" />
+    <link href="./stylesheets/reset.css" type="text/css" rel="stylesheet" />
+</head>
+	<header>
+		
+	</header>
+<body>
+</body>
